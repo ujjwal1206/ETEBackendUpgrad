930c1369765d77cb255cf020a3447579feabc1fc
ujjwal1206 <ujjwal1985.cse18@chitkara.edu.in>
2021-04-28 17:29:04 +0530

    controller part is done

--- src/main/java/com/example/EteBackend/controller/UserController.java
+++ src/main/java/com/example/EteBackend/controller/UserController.java
@@ -3,9 +3,11 @@ package com.example.EteBackend.controller;
 import com.example.EteBackend.model.UserDetails;
 import com.example.EteBackend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+@Controller
 public class UserController {
 
 @Autowired
