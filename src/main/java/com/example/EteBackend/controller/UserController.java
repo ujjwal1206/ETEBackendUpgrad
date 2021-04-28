5c013474a7ded7c98703dbed3874508070280ecf
ujjwal1206 <ujjwal1985.cse18@chitkara.edu.in>
2021-04-28 17:20:21 +0530

    partially completed

---
+++ src/main/java/com/example/EteBackend/controller/UserController.java
@@ -0,0 +1,18 @@
+package com.example.EteBackend.controller;
+
+import com.example.EteBackend.model.UserDetails;
+import com.example.EteBackend.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+public class UserController {
+
+@Autowired
+private UserService userService;
+@RequestMapping(value="users/signup", method= RequestMethod.POST)
+public String registerUser(UserDetails user){
+    userService.registerUser(user);
+    return "details";
+}
+}
